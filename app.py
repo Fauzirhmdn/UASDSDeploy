faf186b4c90744856aa03b64199630f48530aea5
Arya <42996179+Nevedsi@users.noreply.github.com>
2023-05-29 12:23:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,66 +23,66 @@ if (selected == 'Prediksi Penyakit Jantung'):
     # page title
     st.title('Prediksi Gagal Jantung Menggunakan Model XGBoost')
     
-    col1, col2, col3 = st.columns(3)
+    col1 = st.columns(1)
     
     with col1:
         Age = st.text_input('Umur')
 
-    with col2:
+    with col1:
         RestingBP = st.text_input('Tekanan darah saat istirahat')
 
-    with col3:
+    with col1:
         Cholesterol = st.text_input('Serum Kolestoral dalam mg/dl') 
 
     with col1:
         FastingBS = st.text_input('Gula Darah Puasa (Fasting Blood Sugar) : 1 jika FastingBS > 120 mg/dl, 0: selainnya')
 
-    with col2:
+    with col1:
         MaxHR = st.text_input('Detak Jantung Maksimum yang Tercapai (Maximum Heart Rate)')
 
-    with col3:
+    with col1:
          Oldpeak = st.text_input('Oldpeak atau Nilai numerik yang diukur dalam depresi ((Oldpeak asli + 1) x 10')
 
     with col1:
          Sex_F = st.text_input('Kelamin Perempuan (1 jika iya, 0 jika bukan)')
 
-    with col2:
+    with col1:
          Sex_M = st.text_input('Kelamin Laki-laki (1 jika iya, 0 jika bukan)')
 
-    with col3:
+    with col1:
         ChestPaintType_ASY = st.text_input('Tipe nyeri dada ASY (Asymptomatic) (1 jika ada, 0 jika tidak ada)')
 
     with col1:
         ChestPaintType_NAP = st.text_input('Tipe nyeri dada NAP (Non-Anginal Pain) (1 jika ada, 0 jika tidak ada)')
 
-    with col2:
+    with col1:
         ChestPaintType_TA = st.text_input('Tipe nyeri dada TA (Typical Angina) (1 jika ada, 0 jika tidak ada)')
 
-    with col3:
+    with col1:
         ChestPaintType_ATA = st.text_input('Tipe nyeri dada ATA (Atypical Angina) (1 jika ada, 0 jika tidak ada)')
 
     with col1:
         RestingECG_LVH = st.text_input('Istirahat LVH Elektrokardiografi (menunjukkan kemungkinan atau pasti hipertrofi ventrikel kiri menurut kriteria Estes) (1 jika iya, 0 jika tidak)')
 
-    with col2:
+    with col1:
         RestingECG_Normal = st.text_input('Istirahat Elektrokardiografi Normal (1 jika iya, 0 jika tidak)')
 
-    with col3:
+    with col1:
         RestingECG_ST = st.text_input('Istirahat Elektrokardiografi ST yang memiliki kelainan gelombang ST-T (inversi gelombang T dan/atau elevasi atau depresi ST > 0,05 mV) (1 jika iya, 0 jika tidak)')
 
     with col1:
         ExerciseAngina_N = st.text_input('Exercise Induced Angina N (1 jika tidak ada, 0 jika ada)')
 
-    with col2:
+    with col1:
         ExerciseAngina_Y = st.text_input('Exercise Induced Angina Y (1 jika ada, 0 jika tidak ada)')
 
-    with col3:
+    with col1:
         ST_Slope_Down = st.text_input('Kemiringan puncak latihan segmen ST ke bawah (1 jika iya, 0 jika tidak)')
 
     with col1:
         ST_Slope_Flat = st.text_input('Kemiringan puncak latihan segmen ST mendatar (1 jika iya, 0 jika tidak)')
 
-    with col2:
+    with col1:
         ST_Slope_Up = st.text_input('Kemiringan puncak latihan segmen ST ke atas (1 jika iya, 0 jika tidak)')
         
 
@@ -103,7 +103,7 @@ if st.button('Hasil Tes Penyakit Jantung'):
     heart_prediction = heart_diseases_model.predict(input_data_reshaped)
 
     if (heart_prediction[0] == 1):
-          heart_diagnosis = 'Orang tersebut memiliki penyakit jantunge'
+          heart_diagnosis = 'Orang tersebut memiliki penyakit jantung'
     else:
           heart_diagnosis = 'Orang tersebut tidak memiliki penyakit jantung'
         
